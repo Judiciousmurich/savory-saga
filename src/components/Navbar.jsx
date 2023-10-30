463098e8b828f7806e600823f1b052a6f54b73ae
judiciousmurich <murichjudicious45@gmail.com>
2023-10-30 22:37:18 +0300

    feat:add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,7 @@ function Navbar() {
     <div className='navbar'>
         <div className='leftSide' id={openLinks?"open" :"close"}>
             <img src={Logo} />
-            {/* <div className='hidden-links'>
-            <Link to='/'>Home</Link>
-            <Link to='/menu'>Menu</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
-            </div> */}
-        </div>
+                 </div>
         <div className='rightSide'>
             <Link to='/'>Home</Link>
             <Link to='/menu'>Menu</Link>
